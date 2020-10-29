926cfb4b329afd56cad9001e8a68547f900b66f7
daan41114 <rendomgamer41114@gmail.com>
2020-10-29 14:13:48 +0100

    opdracht 2 af gemaakt

--- 02-TilesFolders/toon_lijst.py
+++ 02-TilesFolders/toon_lijst.py
@@ -5,8 +5,8 @@ huidige_map = os.getcwd()
 alle_bestanden = os.scandir(huidige_map)
 
 print("Inhoudsopgave van de map: " + huidige_map)
-for bestand in alle_bestanden:
-    print(bestand.name)
+#for bestand in alle_bestanden:
+    #print(bestand.name)
 
 for bestand in alle_bestanden:
     if bestand.is_file():
